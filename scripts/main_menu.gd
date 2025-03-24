e5c4100d1ac0dee95ea4033b4f8b953288425bbf
Kiberlis <Kiberlis3000@gmail.com>
2025-03-25 04:47:38 +0700

    qq

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,12 +1,29 @@
-extends Control
+class_name MainMenu
+extends  Control
+
+@onready var play_button: Button = %PlayButton
+@onready var options_button: Button = %OptionsButton
+@onready var about_button: Button = %AboutButton
+@onready var exit_button: Button = %ExitButton
+
+const OPTIONS_SCENE: PackedScene = preload("res://scenes/gui/options_menu.tscn")
+
+func _ready() -> void:
+	play_button.pressed.connect(_on_play_pressed)
+	options_button.pressed.connect(_on_options_pressed)
+	about_button.pressed.connect(_on_about_pressed)
+	exit_button.pressed.connect(_on_exit_pressed)
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/level.tscn")
+	print("Start Game")
 
+func _on_options_pressed() -> void:
+	var options_instance = OPTIONS_SCENE.instantiate()
+	get_tree().root.add_child(options_instance)
+	queue_free()
 
-#func _on_settings_pressed() -> void:
-	#pass # Replace with function body.
-
+func _on_about_pressed() -> void:
+	print("Show About")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
---
+++ scripts/options_menu.gd
@@ -0,0 +1,11 @@
+class_name OptionsMenu
+extends  Control
+
+@onready var resolution_dropdown: OptionButton = %Resolution
+@onready var fullscreen_checkbox: CheckBox = %Fullscreen
+@onready var master_volume: HSlider = %MasterVolume
+@onready var music_volume: HSlider = %MusicVolume
+@onready var sfx_volume: HSlider = %SFXVolume
+@onready var reset_button: Button = %ResetButton
+@onready var back_button: Button = %BackButton
+@onready var background: TextureRect = %Background
