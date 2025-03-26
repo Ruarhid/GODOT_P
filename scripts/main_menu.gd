f5fd349982a4ea9e0456b4e44cbd801904a8f081
Kiberlis <Kiberlis3000@gmail.com>
2025-03-26 20:44:35 +0700

    fix
    positon menu item

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,10 +2,10 @@
 class_name MainMenu
 extends Control
 # Объявление и инициализация кнопок
-@onready var play_button: Button = %PlayButton
-@onready var options_button: Button = %OptionsButton
-@onready var about_button: Button = %AboutButton
-@onready var exit_button: Button = %ExitButton
+@onready var play_button: Button = %PlayButton as Button
+@onready var options_button: Button = %OptionsButton as Button
+@onready var about_button: Button = %AboutButton as  Button
+@onready var exit_button: Button = %ExitButton as Button
 
 # Путь к сценам
 const OPTIONS_SCENE: String = "res://scenes/ui/options_menu.tscn"
