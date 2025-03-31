22ff570422c4e701d7c80e9bf83c4fd46a7b01dd
Ruarhid <vladislav-shachnev@yandex.ru>
2025-04-01 03:26:51 +0700

    player

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,31 +1,34 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 50.0
 const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(_delta: float) -> void:
-	# Add the gravity.
-	#if not is_on_floor():
-	#	velocity += get_gravity() * delta
 
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	#	velocity.y = JUMP_VELOCITY
+	if Input.is_action_pressed("ui_right"):
+		$AnimatedSprite2D.flip_h = false
+	if Input.is_action_pressed("ui_left"):
+		$AnimatedSprite2D.flip_h = true
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	
 	var directionx:= Input.get_axis("ui_left", "ui_right")
-	if directionx:
-		velocity.x = directionx * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	var directiony:= Input.get_axis("ui_up", "ui_down")
-	if directiony:
+	if directionx or directiony:
+		velocity.x = directionx * SPEED
 		velocity.y = directiony * SPEED
 	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+# Анимация
+	if velocity.y > 0:  # Движение вверх
+		$AnimatedSprite2D.play("run_front")
+	elif velocity.y < 0:  # Движение вниз
+		$AnimatedSprite2D.play("run_back")
+	elif velocity.x != 0:  # Движение влево или вправо
+		$AnimatedSprite2D.play("run_flip")  # Обычная анимация бега
+		
+
+
 	move_and_slide()
