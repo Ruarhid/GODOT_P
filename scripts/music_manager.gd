bb7838992e10e18e4b7cd42cc1f4ec4de89b7757
Ruarhid <vladislav-shachnev@yandex.ru>
2025-03-31 20:10:03 +0700

    click sound

--- scripts/music_manager.gd
+++ scripts/music_manager.gd
@@ -2,3 +2,9 @@ extends Node
 
 func _ready():
 	$BackgroundMusic.play()
+	
+func _input(event):
+	# Проверяем, был ли клик левой кнопкой мыши
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		# Проигрываем звук
+		$ClickSound.play()
